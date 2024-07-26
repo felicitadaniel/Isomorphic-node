0235bc2bb73305ad4483d46dfe8d5d1dbf14fdae
felicitadaniel <daniel.felicita@gmail.com>
2024-07-26 09:46:56 -0500

    Deploy files

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ module.exports = {
         config.resolve.alias['@'] = path.resolve(__dirname)
         return config
     },
+    assetPrefix:
+        process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+    basePath: '/your-repo-name',
+    trailingSlash: true,
 }
